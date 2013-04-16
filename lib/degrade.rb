fddca81007d31bd38b64ae5e4bcd21987fb58a7f
Laust Rud Jacobsen <laust@object.io>
2013-04-16 22:20:03 +0200

    Degrade: prefer to use attr_reader instead of poking at ivars

--- lib/degrade.rb
+++ lib/degrade.rb
@@ -1,4 +1,9 @@
 class Degrade
+
+  attr_reader :name
+  attr_reader :minimum, :sample, :threshold, :errors, :failure_strategy
+  attr_reader :redis
+
   def initialize(redis, options)
     @redis            = redis
     @name             = options[:name]
@@ -13,50 +18,50 @@ class Degrade
     begin
       mark_request
       yield
-    rescue *@errors => e
+    rescue *errors => e
       mark_failure
       raise e
     end
   end
 
   def requests
-    @redis.get(requests_key).to_f
+    redis.get(requests_key).to_f
   end
 
   def failures
-    @redis.get(failures_key).to_f
+    redis.get(failures_key).to_f
   end
 
   private
     def requests_key
-      "status:#{@name}:requests"
+      "status:#{name}:requests"
     end
 
     def failures_key
-      "status:#{@name}:failures"
+      "status:#{name}:failures"
     end
 
     def mark_request
-      @redis.incr(requests_key)
+      redis.incr(requests_key)
       reset_sample
     end
 
     def mark_failure
-      @redis.incr(failures_key)
+      redis.incr(failures_key)
       check_threshold
     end
 
     def check_threshold
       total_requests = requests
-      return if total_requests < @minimum
-    
-      @failure_strategy.call if failures / total_requests >= @threshold
+      return if total_requests < minimum
+
+      failure_strategy.call if failures / total_requests >= threshold
     end
 
     def reset_sample
-      if requests > @sample
-        @redis.del(requests_key)
-        @redis.del(failures_key)
+      if requests > sample
+        redis.del(requests_key)
+        redis.del(failures_key)
       end
     end
 end
